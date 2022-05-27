094d72e5455f8ecd7642dd3510446e0723a74974
Brian Shimkus <brianshimkus1@gmail.com>
2022-05-26 23:43:30 -0500

    Set up post functionality

--- server/index.js
+++ server/index.js
@@ -1,5 +1,33 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'employeeSystem',
+})
+
+app.post('/create', (req, res) => {
+	const name = req.body.name
+	const age = req.body.age
+	const country = req.body.country
+	const position = req.body.position
+	const wage = req.body.wage
+
+	db.query(
+		'INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)',
+		[name, age, country, position, wage],
+		(err, result) => {
+			if (err) {
+				console.log(err)
+			} else {
+				res.send('Employee created')
+			}
+		}
+	)
+})
 
 app.listen(3001, () => {
 	console.log('Server running on port 3001')
