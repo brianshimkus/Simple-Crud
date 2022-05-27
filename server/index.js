816135841dd304edf1cc26ce3ef5878017ed4e31
Brian Shimkus <brianshimkus1@gmail.com>
2022-05-27 00:26:20 -0500

    Set up cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql')
+const mysql = require('mysql2')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({
 	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'employeeSystem',
+	user: 'brian',
+	password: 'brian',
+	database: 'employee',
 })
 
 app.post('/create', (req, res) => {
